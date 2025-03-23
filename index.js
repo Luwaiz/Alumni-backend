1f0ca6ce442175996632548931f026a0ee6f29f1
Eluwa Emmanuel <emmaeluwa2021@gmail.com>
2025-03-23 21:09:16 +0100

    Memory upload

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/auth", UserRoute);
-app.use("/api/auth/memory", Memory);
+app.use("/api/posts", Memory);
 
 // app.use("/api/expense", ExpenseRoute);
 // app.use("/api/budget", BudgetRoute);
